a10b805760a657c6c253c9adbab510bdc5247480
leVraiNadym <nadym.mallek@gmail.com>
2024-07-13 18:05:25 +0200

    first version user interface and adjust_points in progress

--- img_loader.py
+++ img_loader.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from tkinter.filedialog import askopenfilename, askdirectory
 
 def sort_pts(points):
     sorted_pts = np.zeros((4, 2), dtype="float32")
@@ -36,8 +37,23 @@ class ImgLoader:
         
         self.sorted_pts = sort_pts(self.points)
         self.h_base, self.w_base, self.c_base = self.base_image.shape
-
-
+   
+    def adjust_points(self):
+        #WORK IN PROGRESS
+        print(self.points)
+        
+        for i in range(len(self.points)):
+            curr_point = (self.points[i][0],self.points[i][1])
+            cv2.circle(self.base_image_copy, curr_point, 4, (0,0,255), -1)
+        
+        cv2.line(self.base_image_copy, (self.points[1][0],self.points[1][1]), (self.points[3][0],self.points[3][1]), (0,0,255), 1)
+        cv2.line(self.base_image_copy, (self.points[0][0],self.points[0][1]), (self.points[1][0],self.points[1][1]), (0,0,255), 1)
+        cv2.line(self.base_image_copy, (self.points[3][0],self.points[3][1]), (self.points[2][0],self.points[2][1]), (0,0,255), 1)
+        cv2.line(self.base_image_copy, (self.points[2][0],self.points[2][1]), (self.points[0][0],self.points[0][1]), (0,0,255), 1)
+        cv2.imshow('image', self.base_image_copy)
+        cv2.waitKey(0)
+        pass
+    
     def insert_img(self, img_path : str):
         subject_image = cv2.imread(img_path)
         h_subject, w_subject = subject_image.shape[:2]
@@ -59,13 +75,19 @@ class ImgLoader:
         masked_image = cv2.bitwise_and(self.base_image, inverted_mask)
         output = cv2.bitwise_or(warped_img, masked_image)
 
-        # cv2.imshow('Fused Image', output)
-        name_result = "output_" + img_path[:-4] + ".png"
+        #cv2.imshow('Fused Image', output)
+        name_result = "output_" + img_path[11:-4] + ".png"
         cv2.imwrite(name_result, output)
-        # cv2.waitKey(0)
+        #cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 
-imgLoader = ImgLoader("./base.jpg")
+
+#path_base_img = askopenfilename(title= 'Select the background image')
+#path_dir_subjects = askdirectory(title= 'Select the directory of the subject images')
+path_base_img = "./base.jpg"
+imgLoader = ImgLoader(path_base_img)
 imgLoader.init_points()
+imgLoader.adjust_points()
 imgLoader.insert_img("./subjects/Karimou.png")
+quit()
