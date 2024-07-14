cab56d5b15cd5e0f30a61fbe63a20b81ed4bcb91
kb.bk <karim.belhadj@ledr.io>
2024-07-14 13:21:54 +0100

    fix: points sorting

--- img_loader.py
+++ img_loader.py
@@ -1,19 +1,29 @@
 import numpy as np
 import cv2
 # from tkinter.filedialog import askopenfilename, askdirectory
-from utils import sort_pts, get_closest_point_index
+from utils import (
+    sort_pts,
+    get_closest_point_index,
+    create_overlayed_image
+)
 
 class ImgLoader:
     def __init__(
             self,
             path_base_img   : str,
             path_output_img : str = "./", 
-        ):
+    ) -> None:
         self.path_base_img   = path_base_img
         self.path_output_img = path_output_img
         self.base_image      = cv2.imread("./base.jpg")
         self.base_image_copy = self.base_image.copy()
         self.points          = []
+
+    def update_output_path(
+        self,
+        new_output_path : str
+    ) -> None:
+        self.path_output_img = new_output_path
 
 
     def click_event(self, event, x, y, flags, params):
@@ -23,53 +33,54 @@ class ImgLoader:
             if len(self.points) <= 4:
                 cv2.imshow('image', self.base_image_copy)
 
-    def init_points(self):
+    def init_points(self) -> None:
         cv2.imshow('image', self.base_image_copy)
         cv2.setMouseCallback('image', self.click_event)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         
-        self.sorted_pts = sort_pts(self.points)
+        self.points = sort_pts(self.points)
         self.h_base, self.w_base, self.c_base = self.base_image.shape
 
 
-    def click_update_event(self, event, x, y, flags, params):
+    def click_update_event(self, event, x, y, flags, params) -> None:
         
         if event == cv2.EVENT_LBUTTONDOWN:
             closest_point_index = get_closest_point_index(self.points, [x, y])
             self.points[closest_point_index] = [x,y]
             self.render_box()
    
-    def render_box(self):
+    def render_box(self) -> None:
         self.base_image_copy = self.base_image.copy()
+
         for i in range(len(self.points)):
             curr_point = (self.points[i][0],self.points[i][1])
             cv2.circle(self.base_image_copy, curr_point, 4, (0,0,255), -1)
             
-        cv2.line(self.base_image_copy, (self.points[1][0],self.points[1][1]), (self.points[3][0],self.points[3][1]), (0,0,255), 1)
         cv2.line(self.base_image_copy, (self.points[0][0],self.points[0][1]), (self.points[1][0],self.points[1][1]), (0,0,255), 1)
-        cv2.line(self.base_image_copy, (self.points[3][0],self.points[3][1]), (self.points[2][0],self.points[2][1]), (0,0,255), 1)
-        cv2.line(self.base_image_copy, (self.points[2][0],self.points[2][1]), (self.points[0][0],self.points[0][1]), (0,0,255), 1)
+        cv2.line(self.base_image_copy, (self.points[1][0],self.points[1][1]), (self.points[2][0],self.points[2][1]), (0,0,255), 1)
+        cv2.line(self.base_image_copy, (self.points[2][0],self.points[2][1]), (self.points[3][0],self.points[3][1]), (0,0,255), 1)
+        cv2.line(self.base_image_copy, (self.points[3][0],self.points[3][1]), (self.points[0][0],self.points[0][1]), (0,0,255), 1)
         cv2.imshow('image', self.base_image_copy)
 
-    def adjust_points(self):
+    def adjust_points(self) -> None:
         self.render_box()
         cv2.setMouseCallback('image', self.click_update_event)
         cv2.waitKey(0)
 
     
-    def insert_img(self, img_path : str):
+    def insert_img(self, img_path : str) -> None:
         subject_image = cv2.imread(img_path)
         h_subject, w_subject = subject_image.shape[:2]
 
         pts1 = np.float32([[0, 0], [w_subject, 0], [w_subject, h_subject], [0, h_subject]])
-        pts2 = np.float32(self.sorted_pts)
+        pts2 = np.float32(self.points)
 
         transformation_matrix = cv2.getPerspectiveTransform(pts1, pts2)
         warped_img = cv2.warpPerspective(subject_image, transformation_matrix, (self.w_base, self.h_base))
 
         mask = np.zeros(self.base_image.shape, dtype=np.uint8)
-        roi_corners = np.int32(self.sorted_pts)
+        roi_corners = np.int32(self.points)
         
         filled_mask = mask.copy()
         cv2.fillConvexPoly(filled_mask, roi_corners, (255, 255, 255))
@@ -92,4 +103,7 @@ imgLoader = ImgLoader(path_base_img)
 imgLoader.init_points()
 imgLoader.adjust_points()
 imgLoader.insert_img("./subjects/Karimou.png")
+
+# create_overlayed_image(imgLoader, "https://sideral.ai")
+
 quit()
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from typing import List, Tuple
 
 Point = Tuple[int, int]
 
-def get_closest_point_index(points : List[Point], new_point : Point):
+def get_closest_point_index(points : List[Point], new_point : Point) -> int:
     dist_list = []
     np_new_point = np.array(new_point)
     for point in points:
@@ -13,8 +13,8 @@ def get_closest_point_index(points : List[Point], new_point : Point):
     closes_point_index = np.array(dist_list).argmin()
     return closes_point_index
 
-def sort_pts(points):
-    sorted_pts = np.zeros((4, 2), dtype="float32")
+def sort_pts(points : List[Point]) -> List[Point]:
+    sorted_pts = np.zeros((4, 2), dtype="int32")
     s = np.sum(points, axis=1)
     sorted_pts[0] = points[np.argmin(s)]
     sorted_pts[2] = points[np.argmax(s)]
@@ -22,9 +22,13 @@ def sort_pts(points):
     diff = np.diff(points, axis=1)
     sorted_pts[1] = points[np.argmin(diff)]
     sorted_pts[3] = points[np.argmax(diff)]
-
     return sorted_pts
 
-def screen_web_page(page_url : str, dest_img : str):
+def screen_web_page(website_url : str, dest_img : str) -> None:
     hti = Html2Image(size=(1520, 880))
-    hti.screenshot(url=page_url, save_as=dest_img)
+    hti.screenshot(url=website_url, save_as=dest_img)
+
+def create_overlayed_image(img_loader, website_url : str) -> None:
+    path_website_img = "tmp_website.png"
+    screen_web_page(website_url, path_website_img)
+    img_loader.insert_img(path_website_img)
